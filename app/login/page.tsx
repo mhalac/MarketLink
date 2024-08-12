18272d9d841f02d0e1f9822807ba471bb97189b3
MhalacElRetorno <130261253+MhalacElRetorno@users.noreply.github.com>
2024-08-12 14:17:38 -0300

    Peron peron

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,22 +10,9 @@ export default function Login() {
   function handle_clicks() {
     API_REQUEST("login", "GET", "", requested);
   }
-
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="flex-login">
-        <div>
-          <label className="login-label oswald-login text-4xl">LOGIN</label>
-        </div>
-        <div className="login-div outline outline-slate-100 outline-4 rounded-lg">
-          <button
-            onClick={handle_clicks}
-            className="oswald-login w-32 h-20 text-xl bg-green-400 rounded-lg sticky top-2/3 left-1/4"
-          >
-            {data ? <div>{data}</div> : <div>Loading...</div>}
-          </button>
-        </div>
-      </div>
+    <div className="bg-[url('/background.jpg')] bg-cover bg-center h-screen w-full">
+      
     </div>
   );
 }
