e4f2fe76dbb2b7208639870e23fd46fe1af0a3a1
MhalacElRetorno <130261253+MhalacElRetorno@users.noreply.github.com>
2024-08-13 22:21:39 -0300

    tiene contrase;a de mongo db

--- app/api/login/route.tsx
+++ app/api/login/route.tsx
@@ -1,3 +1,4 @@
+
 import { NextResponse } from "next/server";
 
 // Handles GET requests to /api
@@ -8,8 +9,9 @@ export async function GET(request: Request) {
 
 // Handles POST requests to /api
 export async function POST(request: Request) {
-  // ...
+
   const data = await request.json();
   console.log(data)
   return NextResponse.json({ message: "Hello World" });
+
 }
