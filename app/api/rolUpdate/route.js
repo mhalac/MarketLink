879a29709de798eb6920dd5f0684e83cf3a464be
Matias-Hilaire <matuhilaire@gmail.com>
2024-10-17 22:33:20 -0300

    marron update

--- app/api/rolUpdate/route.js
+++ app/api/rolUpdate/route.js
@@ -2,8 +2,17 @@ import { db } from '../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { nuevoRol } = req.body;
+    const { nrol} = req.body;
 
-      const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nuevoRol, 1]);
-  }}
-  
+    try {
+      const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nrol, 1]);
+
+      res.status(200).json({ message: 'Rol actualizado correctamente' });
+    } catch (error) {
+      console.error('Error al actualizar el rol:', error);
+      res.status(500).json({ error: 'Error al actualizar el rol' });
+    }
+  } else {
+    res.status(405).json({ message: 'Método no permitido' });
+  }
+}
