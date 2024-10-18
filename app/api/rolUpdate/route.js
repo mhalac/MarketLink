f2c4de029fa235eb37590332a4cc4224dcc54a08
Matias-Hilaire <matuhilaire@gmail.com>
2024-10-17 22:44:02 -0300

    ale me esta robando

--- app/api/rolUpdate/route.js
+++ app/api/rolUpdate/route.js
@@ -2,8 +2,8 @@ import { db } from '../../lib/db';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { nuevoRol } = req.body;
+    const { nrol } = req.body;
 
-      const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nuevoRol, 1]);
+      const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nrol, 1]);
   }}
   
