1ed941e320891bb51e967f21bed86e480f0caa8c
MhalacElRetorno <matiasmatute100@gmail.com>
2024-10-31 09:18:58 -0300

    Fixeado?

--- app/api/rolUpdate/route.js
+++ app/api/rolUpdate/route.js
@@ -1,19 +1,15 @@
 import getDB from "@/util/db";
 const db = getDB();
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { nrol } = req.body;
+export async function POST(req, res) {
+  const { nrol } = req.body;
 
-    try {
-      const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nrol, 1]);
-      res.status(200).json({ message: 'Rol actualizado', result });
-    } catch (error) {
-      console.error('Error al actualizar el rol:', error);
-      res.status(500).json({ error: 'Error al actualizar el rol' });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    const result = await db.run('UPDATE usuario SET rol = ? WHERE id_usuario = ?', [nrol, 1]);
+    res.status(200).json({ message: 'Rol actualizado', result });
+  } catch (error) {
+    console.error('Error al actualizar el rol:', error);
+    res.status(500).json({ error: 'Error al actualizar el rol' });
   }
+
 }
